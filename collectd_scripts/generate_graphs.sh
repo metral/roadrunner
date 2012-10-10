39f01cd647d8f68da16e9c3576e3acb2a339142a
Mike Metral <metral@gmail.com>
2012-10-10 13:49:33 -0500

    Consolidated graphing code, added time scope, cleaned up the commands

--- collectd_scripts/generate_graphs.sh
+++ collectd_scripts/generate_graphs.sh
@@ -2,17 +2,56 @@
 
 INPUT_DIR=$1
 OUTPUT_DIR=$2
+START_TIME=$3
+END_TIME=$4
 
-rrdtool graph $OUTPUT_DIR/disk_octets.png -w 800 -h 800 DEF:disk_oct_read=$INPUT_DIR/disk_octets-vda.rrd:read:AVERAGE DEF:disk_oct_write=$INPUT_DIR/disk_octets-vda.rrd:write:AVERAGE LINE1:disk_oct_read#FF0000 LINE1:disk_oct_write#00FF00
+function graph {
 
-rrdtool graph $OUTPUT_DIR/disk_ops.png -w 800 -h 800 DEF:disk_oct_read=$INPUT_DIR/disk_octets-vda.rrd:read:AVERAGE DEF:disk_oct_write=$INPUT_DIR/disk_octets-vda.rrd:write:AVERAGE LINE1:disk_oct_read#FF0000 LINE1:disk_oct_write#00FF00
+    rrdtool graph $OUTPUT_DIR/disk_octets.png -w 800 -h 800 \
+        DEF:disk_oct_read=$INPUT_DIR/disk_octets-vda.rrd:read:AVERAGE \
+        DEF:disk_oct_write=$INPUT_DIR/disk_octets-vda.rrd:write:AVERAGE \
+        LINE2:disk_oct_read#FF0000 \
+        LINE2:disk_oct_write#00FF00 $1 
 
-rrdtool graph $OUTPUT_DIR/if_errors_vnet0.png -w 800 -h 800 DEF:if_errors-RX=$INPUT_DIR/if_errors-vnet0.rrd:rx:AVERAGE DEF:if_errors-TX=$INPUT_DIR/if_errors-vnet0.rrd:tx:AVERAGE LINE1:if_errors-RX#FF0000 LINE1:if_errors-TX#00FF00
+    rrdtool graph $OUTPUT_DIR/disk_ops.png -w 800 -h 800 \
+        DEF:disk_oct_read=$INPUT_DIR/disk_octets-vda.rrd:read:AVERAGE \
+        DEF:disk_oct_write=$INPUT_DIR/disk_octets-vda.rrd:write:AVERAGE \
+        LINE2:disk_oct_read#FF0000 \
+        LINE2:disk_oct_write#00FF00 $1 
 
-rrdtool graph $OUTPUT_DIR/if_dropped_vnet0.png -w 800 -h 800 DEF:if_dropped-RX=$INPUT_DIR/if_dropped-vnet0.rrd:rx:AVERAGE DEF:if_dropped-TX=$INPUT_DIR/if_dropped-vnet0.rrd:tx:AVERAGE LINE1:if_dropped-RX#FF0000 LINE1:if_dropped-TX#00FF00
+    rrdtool graph $OUTPUT_DIR/if_errors_vnet0.png -w 800 -h 800 \
+        DEF:if_errors-RX=$INPUT_DIR/if_errors-vnet0.rrd:rx:AVERAGE \
+        DEF:if_errors-TX=$INPUT_DIR/if_errors-vnet0.rrd:tx:AVERAGE \
+        LINE2:if_errors-RX#FF0000 \
+        LINE2:if_errors-TX#00FF00 $1 
 
-rrdtool graph $OUTPUT_DIR/if_octets_vnet0.png -w 800 -h 800 DEF:if_octets-RX=$INPUT_DIR/if_octets-vnet0.rrd:rx:AVERAGE DEF:if_octets-TX=$INPUT_DIR/if_octets-vnet0.rrd:tx:AVERAGE LINE1:if_octets-RX#FF0000 LINE1:if_octets-TX#00FF00
+    rrdtool graph $OUTPUT_DIR/if_dropped_vnet0.png -w 800 -h 800 \
+        DEF:if_dropped-RX=$INPUT_DIR/if_dropped-vnet0.rrd:rx:AVERAGE \
+        DEF:if_dropped-TX=$INPUT_DIR/if_dropped-vnet0.rrd:tx:AVERAGE \
+        LINE2:if_dropped-RX#FF0000 \
+        LINE2:if_dropped-TX#00FF00 $1 
 
-rrdtool graph $OUTPUT_DIR/if_packets_vnet0.png -w 800 -h 800 DEF:if_packets-RX=$INPUT_DIR/if_packets-vnet0.rrd:rx:AVERAGE DEF:if_packets-TX=$INPUT_DIR/if_packets-vnet0.rrd:tx:AVERAGE LINE1:if_packets-RX#FF0000 LINE1:if_packets-TX#00FF00
+    rrdtool graph $OUTPUT_DIR/if_octets_vnet0.png -w 800 -h 800 \
+        DEF:if_octets-RX=$INPUT_DIR/if_octets-vnet0.rrd:rx:AVERAGE \
+        DEF:if_octets-TX=$INPUT_DIR/if_octets-vnet0.rrd:tx:AVERAGE \
+        LINE2:if_octets-RX#FF0000 \
+        LINE2:if_octets-TX#00FF00 $1 
 
-rrdtool graph $OUTPUT_DIR/virt_cpu_total.png -w 800 -h 800 DEF:cpu_ns=$INPUT_DIR/virt_cpu_total.rrd:ns:AVERAGE LINE1:cpu_ns#FF0000
+    rrdtool graph $OUTPUT_DIR/if_packets_vnet0.png -w 800 -h 800 \
+        DEF:if_packets-RX=$INPUT_DIR/if_packets-vnet0.rrd:rx:AVERAGE \
+        DEF:if_packets-TX=$INPUT_DIR/if_packets-vnet0.rrd:tx:AVERAGE \
+        LINE2:if_packets-RX#FF0000 \
+        LINE2:if_packets-TX#00FF00 $1 
+
+    rrdtool graph $OUTPUT_DIR/virt_cpu_total.png -w 800 -h 800 \
+        DEF:cpu_ns=$INPUT_DIR/virt_cpu_total.rrd:ns:AVERAGE \
+        LINE2:cpu_ns#FF0000 $1 
+
+}
+
+if [ -z != $START_TIME ] && [ -z != $END_TIME ]; then
+    params="--start $START_TIME --end $END_TIME"
+    graph "$params"
+else
+    graph
+fi
